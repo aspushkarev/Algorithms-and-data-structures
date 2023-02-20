c0af2419c56ee6de577feab4d9af406a5d0cca55
aspushkarev <psashok@mail.ru>
2023-02-20 22:36:38 +0300

    Added solution Junior level

---
+++ Lucky_Tickets/Lucky_Tickets.py
@@ -0,0 +1,21 @@
+'''
+Junior level
+Посчитать, сколько существует счастливых 6-значных билетов
+O(10^N)
+'''
+
+
+def lucky_tickets():
+    score = 0
+    for a1 in range(10):
+        for a2 in range(10):
+            for a3 in range(10):
+                for b1 in range(10):
+                    for b2 in range(10):
+                        for b3 in range(10):
+                            if a1 + a2 + a3 == b1 + b2 + b3:
+                                score += 1
+    print('Счастливых 6-значных билетов', score)
+
+
+lucky_tickets()
