4968c08e1afbaa9aa9fd9866e3798e0a1b0d53d7
aspushkarev <psashok@mail.ru>
2023-03-18 00:26:57 +0300

    Added solution with Insertion sorting

--- simple_sorting/simple_sorting.py
+++ simple_sorting/simple_sorting.py
@@ -5,23 +5,23 @@ from array import array
 random.seed(154)
 
 
-def init(n) -> array:
+def init(n):
     """
     Функция инициализирует массив целых чисел
     диапазоном от 1 до n
     :param n: Целое число для инициализации размера массива
     :return: Массив со случайно сгенерированными целыми числами
     """
-    arr = array('L', range(n))
-    for i in range(n):
-        arr[i] = random.randint(1, n)
-    return arr
+    return array('l', (random.randint(1, n) for i in range(n)))
 
 
 def bubble_sort(array, n):
     """
     Функция сортировки пузырьковым методом
-    :return:
+    O(N^2)
+    :param array: Массив для сортировки
+    :param n: Число элементов массива
+    :return: Отсортированный массив, число сравнений и число вставок
     """
     compare = 0
     assignment = 0
@@ -31,11 +31,45 @@ def bubble_sort(array, n):
                 compare += 1
                 assignment += 2
                 array[i], array[i + 1] = array[i + 1], array[i]
-    return arr, compare, assignment
+    return array, compare, assignment
 
 
-n = 10
-arr = init(n)
-start = time.process_time()
-array, compare, assignment = bubble_sort(arr, n)
-print(f'N: {n}\t compare: {compare}\t assignment: {assignment} \t {(time.process_time() - start):.5f} seconds')
+def insertion_sort(array, n):
+    """
+    Функция сортировки методом вставки
+    O(N^2)
+    :param array: Массив для сортировки
+    :param n: Число элементов массива
+    :return: Отсортированный массив, число сравнений и число вставок
+    """
+    compare = 0
+    assignment = 0
+    for j in range(n):
+        for i in range(j - 1, -1, -1):
+            if array[i] > array[i + 1]:
+                compare += 1
+                assignment += 2
+                array[i], array[i + 1] = array[i + 1], array[i]
+    return array, compare, assignment
+
+
+numbers = [10, 100, 1000, 10_000]
+print('Bubble sorted...')
+for n in numbers:
+    # arr = init(n)
+    arr = [random.randint(1, n) for i in range(n)]
+    # print(arr[0:20])
+    start = time.process_time()
+    array, compare, assignment = bubble_sort(arr, n)
+    print(f'N: {n}\t compare: {compare}\t assignment: {assignment} \t {(time.process_time() - start):.5f} seconds')
+    # print(array[0:20])
+
+print('\nInsertion sorted...')
+for n in numbers:
+    # arr2 = init(n) 
+    arr2 = [random.randint(1, n) for j in range(n)]
+    # print(arr2[0:20])
+    start = time.process_time()
+    array, compare, assignment = insertion_sort(arr2, n)
+    print(f'N: {n}\t compare: {compare}\t assignment: {assignment} \t {(time.process_time() - start):.5f} seconds')
+    # print(array[0:20])
